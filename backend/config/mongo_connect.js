e21ce488f4b0944427977475e0093cafc79ff032
narukaz <85193271+narukaz@users.noreply.github.com>
2025-03-16 03:28:35 +0530

    updating the mongo address

--- backend/config/mongo_connect.js
+++ backend/config/mongo_connect.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 
 export function connectToMongo(){
-    return mongoose.connect("mongodb://localhost:27017/data").then((data)=>{
+    return mongoose.connect("mongodb+srv://test:abcd@paper.17km0ug.mongodb.net/?retryWrites=true&w=majority&appName=paper").then((data)=>{
         console.log("Connected to mongo")
     }).catch(err=>{
         console.log("Error connecting mongo:",err?.message)
