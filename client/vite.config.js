fbcfc5219355173e7cb7861ff07baea7dda0b641
narukaz <85193271+narukaz@users.noreply.github.com>
2025-03-16 02:27:31 +0530

    changing server in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '192.168.29.10', // or '0.0.0.0' to listen on all addresses
-    port: 5173,
-  },
+ 
   plugins: [react(),tailwindcss(),],
 })
