4f52cefc8467bba653870b39c6a4d6cd14fcde8a
narukaz <85193271+narukaz@users.noreply.github.com>
2025-03-16 23:30:20 +0530

    ngrock

--- client/src/slices/login-slice.js
+++ client/src/slices/login-slice.js
@@ -10,7 +10,7 @@ let initialState={
 
 export const GetUser = createAsyncThunk("user/getUser",async(parameter)=>{
   axios.defaults.withCredentials=true
-  const {data} = await axios.get("https://pac-instructors-desktops-responsibilities.trycloudflare.com/user/getUser",{
+  const {data} = await axios.get("https://297c-3-110-204-158.ngrok-free.app/user/getUser",{
       headers: {
         "Content-Type": "application/json"
       }
@@ -21,7 +21,7 @@ export const GetUser = createAsyncThunk("user/getUser",async(parameter)=>{
 
 export const UserLogin = createAsyncThunk("user/login",async(parameter)=>{
     axios.defaults.withCredentials=true
-    const {data} = await axios.post("https://pac-instructors-desktops-responsibilities.trycloudflare.com/user/login", {...parameter},{
+    const {data} = await axios.post("https://297c-3-110-204-158.ngrok-free.app/user/login", {...parameter},{
         headers: {
           "Content-Type": "application/json"
         }
@@ -32,7 +32,7 @@ export const UserLogin = createAsyncThunk("user/login",async(parameter)=>{
 
 export const UserLogout = createAsyncThunk("user/logout",async()=>{
   axios.defaults.withCredentials=true
-  const {data} = await axios.post("https://pac-instructors-desktops-responsibilities.trycloudflare.com/user/logout", {
+  const {data} = await axios.post("https://297c-3-110-204-158.ngrok-free.app/user/logout", {
       headers: {
         "Content-Type": "application/json"
       }
@@ -43,7 +43,7 @@ export const UserLogout = createAsyncThunk("user/logout",async()=>{
 
 export const UserSignup = createAsyncThunk("user/signup", async(parameter)=>{
     axios.defaults.withCredentials=true
-    const {data} = await axios.post("https://pac-instructors-desktops-responsibilities.trycloudflare.com/user/signup", {...parameter})
+    const {data} = await axios.post("https://297c-3-110-204-158.ngrok-free.app/user/signup", {...parameter})
     return data
 })
 
