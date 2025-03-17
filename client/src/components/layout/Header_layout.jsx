f03291102def63cbe4c9e5ac7b13057ab2a1d69c
narukaz <85193271+narukaz@users.noreply.github.com>
2025-03-18 00:24:34 +0530

    header fixing

--- client/src/components/layout/Header_layout.jsx
+++ client/src/components/layout/Header_layout.jsx
@@ -22,7 +22,7 @@ function Header_layout() {
     lg:px-9 px-2 gap-4
     flex items-center justify-around`} >
       <h1 className={`${day? "text-black" : "text-white"} font-mono`}>Task Manager</h1>
-      <div>
+      <div className='flex items-center justify-around'>
       
       <div className='flex h-[50px] items-center justify-evenly border px-2 gap-2 rounded-xl '>
         <p className='font-bold'>{user}</p>
