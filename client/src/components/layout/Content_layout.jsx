0f18110a9a251c45baa6124bdbcb88b619ca490a
narukaz <85193271+narukaz@users.noreply.github.com>
2025-03-18 04:27:16 +0530

    addinng scroll in my phone

--- client/src/components/layout/Content_layout.jsx
+++ client/src/components/layout/Content_layout.jsx
@@ -100,7 +100,7 @@ function Content_layout() {
   };
 
   return (
-    <div className="lg:p-10 pt-[34px] px-3 mt-[50px] lg:w-full w-[1000px] h-[100vh] flex justify-between gap-5 overflow-x-scroll">
+    <div className="lg:p-10 pt-[34px] px-3 mt-[50px] lg:w-full w-full h-[100vh] flex justify-between gap-5 overflow-x-scroll">
       {isNewCard && (
         <CardModal
           onCancel={resetForm}
