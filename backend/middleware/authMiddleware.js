2bd50d23a3da47f0100d06a0347c27abf7a7361d
narukaz <85193271+narukaz@users.noreply.github.com>
2025-03-18 04:00:02 +0530

    authorization change

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 export const authentication =(req,res,next) =>{
-    const token = req?.cookies.token;
+    const token = req?.cookies.token || req.headers["authorization"]?.split(" ")[1];;
     if (!token) {
         return res.status(401).json({ error: true, message: "re-Login no token found", redirect: '/login' });
     }
