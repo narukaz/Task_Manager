cd40d175521acecbed06ad17df84214aee7afb4f
narukaz <85193271+narukaz@users.noreply.github.com>
2025-03-17 22:40:21 +0530

    fixing redirection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,21 @@
-import { Outlet, Routes, Route, Navigate } from "react-router-dom";
-import Login_page from "./components/login/Login_page";
-import Signup_page from "./components/signup/Signup_page";
-import Header_layout from "./components/layout/Header_layout";
-import Content_layout from "./components/layout/Content_layout";
-import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-
 function App() {
   const { isAuthenticated } = useSelector((state) => state.auth);
   const { day } = useSelector((state) => state.theme);
-  const token = document.cookie.includes("token=")
+  const [token, setToken] = useState(false);
 
- 
+  useEffect(() => {
+    setToken(document.cookie.includes("token="));
+  }, []);
 
   return (
     <div className={`${day ? "bg-white" : "bg-gray-900"} lg:h-[100vh] w-full lg:overflow-hidden overflow-x-scroll`}>
       <Routes>
         <Route path="/" element={token ? <Navigate to="/dashboard" /> : <Login_page />} />
-
         <Route path="/login" element={token ? <Navigate to="/dashboard" /> : <Login_page />} />
-
         <Route path="/signup" element={token ? <Navigate to="/dashboard" /> : <Signup_page />} />
-
-        <Route path="/dashboard" element={token ? (
+        <Route
+          path="/dashboard"
+          element={token ? (
             <>
               <Header_layout />
               <Content_layout />
@@ -35,5 +28,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
